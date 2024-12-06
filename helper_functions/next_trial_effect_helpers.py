d270664d5c36a87b7dbb7a998f3082781603e17e
Francesca <fmjgreenstreet@gmail.com>
2024-12-06 19:22:14 +0000

    started working on regression for previous choice

---
+++ helper_functions/next_trial_effect_helpers.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+
+def get_previous_choice_same_stim(all_state_changes, APEs, RPEs, choice='Left', stim='High'):
+    left_choices = all_state_changes[all_state_changes['state name'] == stim + choice]
+    left_trial_numbers = left_choices['trial number'].values
+    left_cue_time_stamps = all_state_changes[all_state_changes['state name'] == stim + choice]['time stamp'].values
+    left_action_time_stamps = left_choices['time stamp'].values
+    same_stim_trials = all_state_changes[
+                    (all_state_changes['state name'] == stim + 'Left') |
+                    (all_state_changes['state name'] == stim + 'Right')]
+    last_same_stim_trial_numbers = []
+    last_stim_actions = []
+    last_stim_lags = []
+    APE_trial_numbers = []
+    left_APEs = []
+    left_RPEs = []
+    for i, left_trial_number in enumerate(left_trial_numbers):
+        for lag in range(1, 6):
+            previous_same_stim_trials = same_stim_trials[same_stim_trials['trial number'] < left_trial_number]
+            if previous_same_stim_trials.shape[0] >= lag:
+                last_same_stim_trial_number = previous_same_stim_trials['trial number'].values[-lag]
+                last_same_stim_trial_numbers.append(last_same_stim_trial_number)
+                last_same_stim_trial = all_state_changes[all_state_changes['trial number'] == last_same_stim_trial_number]
+                action_state = last_same_stim_trial[
+                    (last_same_stim_trial['state name'] == stim + 'Left') |
+                    (last_same_stim_trial['state name'] == stim + 'Right')]
+                action = action_state['action taken'].values[0]
+                last_stim_actions.append(action)
+                last_stim_lags.append(lag)
+                APE_trial_numbers.append(left_trial_number)
+                left_APEs.append(APEs[left_action_time_stamps[i]])
+                left_RPEs.append(RPEs[left_cue_time_stamps[i]])
+    past_trial_df = pd.DataFrame({'previous trial number': last_same_stim_trial_numbers, 'lag': last_stim_lags, 'previous action': last_stim_actions,
+                                  'current trial number': APE_trial_numbers, 'current APE': left_APEs, 'current RPE': left_RPEs})
+    return past_trial_df
+
+def run_multilag_regression_past_choice_on_signal(previous_choice_df, num_lags = 5, y_var='current APE'):
+    coefs = np.zeros([num_lags])
+    pvals = np.zeros([num_lags])
+    for lag in range(1, num_lags + 1):
+        lag_df = previous_choice_df[previous_choice_df['lag'] == lag]
+        df = lag_df[[y_var, 'previous action']].copy()
+        df = df.replace({'Left': 1, 'Right': 0})
+
+        df.loc[:, 'previous action'] = df['previous action'].astype(float)
+        df.loc[:, y_var] = df[y_var].apply(
+            lambda x: np.nan if isinstance(x, np.ndarray) and x.size == 0 else x)
+
+        df.loc[:, y_var] = df[y_var].astype(float)
+        df = df.dropna().reset_index(drop=True)
+
+        y = df[y_var].astype(float)
+        X = df['previous action']
+        X = sm.add_constant(X)
+        # Fit the regression model
+        model = sm.OLS(y, X).fit()
+        coefs[lag - 1] = model.params[1:]
+        pvals[lag - 1] = model.pvalues[1:]
+    return coefs, pvals
---
+++ one_agent_classic_task_previous_choice_regression.py
@@ -0,0 +1,106 @@
+import matplotlib.pyplot as plt
+from tqdm import trange
+from agent import Mouse
+from classic_task import Box
+from helper_functions.plotting_functions import *
+from helper_functions.trial_matched_comparisons import get_early_mid_late_left_trials
+from helper_functions.next_trial_effect_helpers import get_previous_choice_same_stim, run_multilag_regression_past_choice_on_signal
+np.random.seed(0)
+
+"""
+Runs simulation for one agent, all models of dopamine. Used to produce 'early mid late' plots seen in fig S7 C, D & E
+"""
+
+if __name__ == '__main__':
+    import pandas as pd
+
+    n_trials = 2000
+    x = np.linspace(0,50, n_trials * 10)
+    cue_reaction_times = np.random.geometric(0.01, x.shape[0])
+    movement_times = np.ones(x.shape[0]) * 2 #np.random.geometric(0.01, x.shape[0]) * 2 for almost all simulations but not for regression
+    e = Box(punish=True)
+    a = Mouse(cue_reaction_times, movement_times, env=e, critic_learning_rate=0.005, actor_learning_rate=0.005, habitisation_rate=0.01, psi=0.2)
+
+    all_PEs = []
+    all_APEs = []
+    all_trial_types = []
+    all_actions = []
+    all_states = []
+    all_state_changes = pd.DataFrame(columns=['state name', 'time stamp', 'dwell time', 'action taken'])
+
+    all_rewards, all_MSs, all_Ns, all_Ss, all_Vs = [], [], [], [], []
+    with trange(n_trials) as t:
+        for i in t:
+            _, PEs, trial_type, action, states, state_changes, apes, trial_r, m_signals, values, novelties, saliences, _ = a.one_trial(i)
+            # pdb.set_trace()
+            all_rewards.append(trial_r)
+            mean_r = sum(all_rewards) / (i + 1.)
+            t.set_description(f"avg. reward = {mean_r}")
+            e.reset()
+            all_states.append(states)
+            all_PEs.append(PEs)
+            all_trial_types.append(trial_type)
+            #################################
+            all_MSs.append(m_signals)
+            all_Ns.append(novelties)
+            all_Ss.append(saliences)
+            all_Vs.append(values)
+            ################################
+            all_actions.append(action)
+            all_state_changes = all_state_changes.append(state_changes, ignore_index=True)
+            all_APEs.append(apes)
+
+continuous_time_PEs = np.concatenate(all_PEs).ravel()
+continuous_time_APEs = np.concatenate(all_APEs).ravel()
+continuous_time_MSs = np.squeeze(np.concatenate(all_MSs))[:, 0]
+continuous_time_Ns = np.squeeze(np.concatenate(all_Ns))
+continuous_time_Ss = np.squeeze(np.concatenate(all_Ss))
+continuous_time_Vs = np.squeeze(np.concatenate(all_Vs))
+
+reward_times = np.where(np.asarray(a.reward_history) == 1)[0]
+
+left_inds = all_state_changes[all_state_changes['action taken'] == 'Left'].index
+cue_left = all_state_changes.shift(periods=1).iloc[left_inds]
+cue_left_times = cue_left['time stamp'].values
+
+right_inds = all_state_changes[all_state_changes['action taken'] == 'Right'].index
+cue_right = all_state_changes.shift(periods=1).iloc[left_inds]
+cue_right_times = cue_right['time stamp'].values
+
+
+low_tone_times = all_state_changes['time stamp'][
+    all_state_changes[all_state_changes['state name'] == 'HighLeft'].index.values].values
+high_tone_times = all_state_changes['time stamp'][
+    all_state_changes[all_state_changes['state name'] == 'LowRight'].index.values].values
+left_choices = all_state_changes['time stamp'][
+    all_state_changes[all_state_changes['action taken'] == 'Left'].index.values].values
+right_choices = all_state_changes['time stamp'][
+    all_state_changes[all_state_changes['action taken'] == 'Right'].index.values].values
+
+incorrect_left_trials = all_state_changes['trial number'][
+    all_state_changes[all_state_changes['state name'] == 'LowLeft'].index.values].values
+correct_left_trials = all_state_changes['trial number'][
+    all_state_changes[all_state_changes['state name'] == 'HighLeft'].index.values].values
+early_incorrect_times, mid_incorrect_times, late_incorrect_times = get_early_mid_late_left_trials(incorrect_left_trials, n_trials, all_state_changes)
+early_correct_times, mid_correct_times, late_correct_times = get_early_mid_late_left_trials(correct_left_trials, n_trials, all_state_changes)
+
+
+states = ['Left cues', 'Right cues', 'Reward', 'Contra', 'Ipsi']
+time_stamps = {'Left cues': cue_left_times, 'Right cues': cue_right_times, 'Reward': reward_times,
+               'Contra': left_choices, 'Ipsi': right_choices}
+models = {'APE': continuous_time_APEs, 'RPE': continuous_time_PEs, 'Novelty': np.sum(continuous_time_Ns, axis=1),
+          'Salience': np.sum(continuous_time_Ss, axis=1), 'Movement': continuous_time_MSs}
+left_choices_previous_choice = get_previous_choice_same_stim(all_state_changes, continuous_time_APEs, continuous_time_PEs, choice='Left', stim='High')
+tail_coefs, tail_pvals = run_multilag_regression_past_choice_on_signal(left_choices_previous_choice, y_var='current APE')
+vs_coefs, vs_pvals = run_multilag_regression_past_choice_on_signal(left_choices_previous_choice, y_var='current RPE')
+fig, ax = plt.subplots(1,1, figsize=(3, 3))
+
+ax.plot(tail_coefs, color='#00343a')
+ax.plot(vs_coefs, color='#E95F32')
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+ax.spines['left'].set_visible(False)
+
+plt.tight_layout()
+plt.show()
